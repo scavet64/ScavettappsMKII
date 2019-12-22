829f3fade90a61f0ab07a7a3b80ccc62def6285e
Vincent Scavetta <vstro24@gmail.com>
2019-12-22 02:13:16 -0500

    Bringing existing design into angular app

---
+++ scavettapps/src/app/homepage/homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewChild, AfterContentInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
